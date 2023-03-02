5977b1b1b212fc68a9226f5b5ececb2b4cc7c294
rhythmxqq <96514857+rhythmxqq@users.noreply.github.com>
2023-03-02 19:04:03 +0300

    123
    123

---
+++ ArrayRazmernPols/ArrayRazmernPols/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите размеры массива через запятую: ");
+        string input = Console.ReadLine();
+        string[] sizes = input.Split(',');
+        int dimensions = sizes.Length;
+
+        int totalSize = 1;
+        int[] dimensionsSizes = new int[dimensions];
+
+        for (int i = 0; i < dimensions; i++)
+        {
+            dimensionsSizes[i] = int.Parse(sizes[i]);
+            totalSize *= dimensionsSizes[i];
+        }
+
+        Console.WriteLine($"Количество элементов в массиве: {totalSize}");
+
+        int[] indices = new int[dimensions];
+
+        for (int i = 0; i < totalSize; i++)
+        {
+            Console.Write("1 ");
+            System.Threading.Thread.Sleep(10);
+            for (int j = dimensions - 1; j >= 0; j--)
+            {
+                indices[j]++;
+
+                if (indices[j] < dimensionsSizes[j])
+                {
+                    break;
+                }
+                indices[j] = 0;
+            }
+
+            if (i % dimensionsSizes[dimensions - 1] == 0)
+            {
+                Console.WriteLine();
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
